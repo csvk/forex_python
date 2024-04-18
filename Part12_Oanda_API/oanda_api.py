3b81b6640e90705aa3f7344074afec96cf9925e7
csvk <chanda.souvik@gmail.com>
2024-04-18 11:14:42 +0100

    api creds access mode change

--- Part12_Oanda_API/oanda_api.py
+++ Part12_Oanda_API/oanda_api.py
@@ -1,6 +1,21 @@
+import json
+API_CREDS_FILE = 'D:/OneDrive/Studies/forex_python/api_creds.json'
+
+# Load API credentials
+with open(API_CREDS_FILE) as json_file:
+    apicreds = json.load(json_file)
+
+class defs():
+    API_KEY = apicreds['API_KEY']
+    ACCOUNT_ID = apicreds['ACCOUNT_ID']
+    OANDA_URL = apicreds['OANDA_URL']
+
+    SECURE_HEADER = {
+        'Authorization': f'Bearer {API_KEY}'
+}
+
 import requests
 import pandas as pd
-import defs
 import utils
 
 class OandaAPI():
